b053074425769c9b3fb30bf4c4b0dfcb71eb27b1
Austin Caron <austin.caron1@gmail.com>
2020-09-14 10:17:04 -0400

    db fix

--- config/config.php
+++ config/config.php
@@ -1,16 +1,16 @@
 <?php 
 
     $db_dsn = array(
-        'host' => 'localhost',
-        'dbname' => 'db_thornton',
+        'host' => 'us-cdbr-east-02.cleardb.com',
+        'dbname' => 'heroku_e9327b77dc02820',
         'charset' => 'utf8',
     );
     
     $dsn = 'mysql:' . http_build_query($db_dsn, '', ';');
 
     // Set up connection credentials
-    $db_user = 'root';
-    $db_pass = '';
+    $db_user = 'b9d2d12673dc61';
+    $db_pass = '6b32b1e3';
 
     $pdo = new PDO($dsn, $db_user, $db_pass);
 
